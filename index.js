8d962f092ac663b79b1267842aec3b68f4d5e74a
TonyChoi19 <kingyip26@gmail.com>
2022-11-07 04:35:42 +0800

    login

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const express = require("express");
-const session = require("express-session");
+const cookieSession = require("cookie-session");
 const env = require("dotenv").config();
 const auth = require("./auth");
 const passport = require("passport");
 const cors = require("cors");
+const { authenticate } = require("passport");
 
 const app = express();
-app.use(session({ secret: "cats" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    name: "session",
+    keys: ["key"],
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors());
 
 //functions
 const isLogged = (req, res, next) => {
+  // req.user ? next() : res.json({ message: "not authenticated" });
   req.user ? next() : res.sendStatus(401);
 };
 
 //GET requests
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
@@ -27,23 +39,19 @@ app.get(
   passport.authenticate("google", {
     successRedirect: "http://localhost:3000/auth/google/success",
     failureRedirect: "http://localhost:3000/auth/google/failure",
-    successMessage: "Logged with Google successfully",
-    failureMessage: "Fail to login with Google",
   })
 );
 
 app.get("/auth/google/user", isLogged, (req, res) => {
+  console.log("user login");
   res.json(req.user);
 });
 
-app.get("/auth/google/failure", (req, res) => {
-  res.send("Error!!");
-});
-
-app.get("/logout", (req, res) => {
+app.get("/auth/google/logout", (req, res) => {
   req.logout();
-  req.session.destroy();
-  res.send("Goodbye!");
+  req.session = null;
+  console.log("user logout");
+  res.redirect("/");
 });
 
 //Listen
